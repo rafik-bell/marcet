cc35c96e67181491a8e91d911a0db25f8f63baac
Bellarbi Rafik <bellarbi.rafik7@gmail.com>
2024-07-30 16:20:27 +0100

    first commit4

--- src/components/Homemarcet/Homemarcet.tsx
+++ src/components/Homemarcet/Homemarcet.tsx
@@ -1,9 +1,35 @@
 import React, { useEffect } from 'react';
-
+import axios from 'axios';
+import {useNavigate} from "react-router-dom";
 import Allprodect from '../myprodect/Allprodect.tsx';
 
 export default function Homemarcet() {
-  
+    const navigate = useNavigate();
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/user/user", {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include'
+                });
+
+                if (!response.ok) {
+                    navigate("/login")
+                }
+
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData(); // Call the async function
+
+    }, []);
+
     return (
         <div>
            <Allprodect></Allprodect>
