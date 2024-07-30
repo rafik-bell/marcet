7524b47179bc335cdd087f121b79f3098e16a544
Bellarbi Rafik <bellarbi.rafik7@gmail.com>
2024-07-30 12:16:33 +0100

    first commit1

--- src/layout/Layoutuser.tsx
+++ src/layout/Layoutuser.tsx
@@ -1,11 +1,41 @@
 // src/components/Layout.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar.tsx';
 import Footer from '../components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import Navbaruser from '../components/Navbar/Navbaruser.tsx';
+import axios from 'axios';
+import {useNavigate} from "react-router-dom";
 
 const Layoutuser = ({ children }) => {
+    const navigate = useNavigate();
+   
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/user/user", {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        
+                    },
+                    credentials: 'include'
+                });
+
+                if (!response.ok) {
+                    navigate("/login")
+                }
+
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData(); // Call the async function
+
+    }, []);
+
     return (
         <div>
             <Navbaruser />
